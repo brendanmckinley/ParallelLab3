d7143c4978e9b4f6767a0ba08e43b32ad1a62cbf
Brendan <brendan.mckinley@eagles.oc.edu>
2019-12-05 16:38:28 -0600

    Update lab files

---
+++ cmake-build-debug/CMakeFiles/Lab3-Fractal.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lab3-Fractal.dir/main.c.o"
+  "Lab3-Fractal"
+  "Lab3-Fractal.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Lab3-Fractal.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
